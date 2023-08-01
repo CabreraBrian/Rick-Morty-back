366e8f644dff221b66aeda4d74415bb849424ef0
CabreraBrian <cabrerabriantbj@gmail.com>
2023-08-01 14:54:22 -0300

    commit deploy

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const server = require("./app")
-const PORT = 3001
 const { conn } = require('./DB_connection');
+const PORT = process.env.PORT || 3001;
 
 conn.sync({force: false}).then(()=>{
-    server.listen(PORT, ()=>console.log(`Listening on port ${PORT}`))
-})
+    server.listen(PORT, ()=> {
+        console.log(`Listening on port ${PORT}`)
+    });
+});
 
